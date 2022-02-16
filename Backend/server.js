1404293b93195b9f728e4885e95587e120dfc8ab
jonher16 <1999.jon@gmail.com>
2022-02-16 09:20:11 +0100

    Front style fix

--- Backend/server.js
+++ Backend/server.js
@@ -1,71 +1,69 @@
 //SOCKET.IO SERVER REQUIREMENTS
 
-const http = require('http'); //Require http to create a http server
+const http = require("http"); //Require http to create a http server
 const socketIo = require("socket.io"); //Packet to send commands over SocketIo
 
 //AXIS REQUIREMENTS
 
-var Axis = require('./Axis.js');
-var axis = new Axis("212.170.116.46","root","pass",{'camera':'1'});
-
+var Axis = require("./Axis.js");
+var axis = new Axis("212.170.116.46", "root", "pass", { camera: "1" });
 
 //Socket.io Server Declarations
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 4001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('SocketIo Server');
-  
+  res.setHeader("Content-Type", "text/plain");
+  res.end("SocketIo Server");
 });
 
 const io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 //Stream declarations
 
 Stream = require("node-rtsp-stream");
+var stream;
 
 //Variables
 
-var zoomstep = 100
-var angle = 20
-var ip = "127.0.0.1"
-var username = "root"
-var password = "root"
-
+var zoomstep = 100;
+var angle = 20;
+var ip = "127.0.0.1";
+var username = "root";
+var password = "root";
 
 //Socket.io Sockets
 
 io.on("connection", (socket) => {
-
-  console.log("A user has connected")
+  console.log("A user has connected");
   io.emit("welcome", "Welcome, new user");
-  
+
   socket.on("camera", (msg) => {
-    ip = msg.ip
-    username = msg.username
-    password = msg.password
+    ip = msg.ip;
+    username = msg.username;
+    password = msg.password;
 
-    console.log(msg.ip)
-    console.log(msg.username)
-    console.log(msg.password)
-    console.log(`rtsp://${username}:${password}@${ip}/axis-media/media.amp`)
+    console.log(msg.ip);
+    console.log(msg.username);
+    console.log(msg.password);
+    console.log(`rtsp://${username}:${password}@${ip}/axis-media/media.amp`);
 
-    axis = new Axis(msg.ip,msg.username,msg.password,{'camera':'1'});
+    axis = new Axis(msg.ip, msg.username, msg.password, { camera: "1" });
     io.emit("welcome", `Axis camera credentials changed.`);
 
-    stream = new Stream({
+    this.stream = new Stream({
       name: "Stream",
       // streamUrl: "rtsp://YOUR_IP:PORT",
       streamUrl: `rtsp://${username}:${password}@${ip}/axis-media/media.amp`,
       wsPort: 6789,
-      ffmpegOptions: { // options ffmpeg flags
+      ffmpegOptions: {
+        // options ffmpeg flags
         "-f": "mpegts", // output file format.
         "-codec:v": "mpeg1video", // video codec
         "-b:v": "1000k", // video bit rate
@@ -80,71 +78,72 @@ io.on("connection", (socket) => {
         "-b:a": "128k", // audio bit rate
       },
     });
-
-    axis.ptz.rpan('50').then(async (res)=>{
-      console.log(res);   //true 
+    io.emit("welcome", `stream => ${this.stream}`);
+    axis.ptz.rpan("50").then(async (res) => {
+      console.log(res); //true
       axis.ptz.reachPos().then((reached) => {
-          console.log(reached)  // true when reached
-      })
-  })
-
+        console.log(reached); // true when reached
+      });
+    });
   });
-  
-  socket.on("command", (command) => {
-    console.log(command)
-    switch(command){
-      case "zoomin":
-      axis.ptz.rzoom(zoomstep)
-      break;
-      case "zoomout":
-      axis.ptz.rzoom(-zoomstep)
-      break;
-      case "left":
-      axis.ptz.rpan(-angle)
-      break;
-      case "right":
-      axis.ptz.rpan(angle)
-      break;
-      case "up":
-      axis.ptz.rtilt(angle)
-      break;
-      case "down":
-      axis.ptz.rtilt(-angle)
-      break;
-      case "upspeed":
-      axis.ptz.continuouspantiltmove(0, 10)
-      break;
-      case "downspeed":
-      axis.ptz.continuouspantiltmove(0, -10)
-      break;
-      case "leftspeed":
-      axis.ptz.continuouspantiltmove(-10, 0)
-      break;
-      case "rightspeed":
-      axis.ptz.continuouspantiltmove(10, 0)
-      break;
-      case "stopspeed":
-      axis.ptz.continuouspantiltmove(0,0)
-      break;
-      case "zoominspeed":
-      axis.ptz.continuouszoommove(10)
-      break;
-      case "zoomoutspeed":
-      axis.ptz.continuouszoommove(-10)
-      break;
-      case "stopzoomspeed":
-      axis.ptz.continuouszoommove(0)
-      break;
-    }
 
+  socket.on("deletestream", () => {
+    io.emit("welcome", `stream end => ${this.stream}`);
+    this.stream.destroy();
+    console.log("Stream closed.");
+  });
+
+  socket.on("command", (command) => {
+    console.log(command);
+    switch (command) {
+      case "zoomin":
+        axis.ptz.rzoom(zoomstep);
+        break;
+      case "zoomout":
+        axis.ptz.rzoom(-zoomstep);
+        break;
+      case "left":
+        axis.ptz.rpan(-angle);
+        break;
+      case "right":
+        axis.ptz.rpan(angle);
+        break;
+      case "up":
+        axis.ptz.rtilt(angle);
+        break;
+      case "down":
+        axis.ptz.rtilt(-angle);
+        break;
+      case "upspeed":
+        axis.ptz.continuouspantiltmove(0, 10);
+        break;
+      case "downspeed":
+        axis.ptz.continuouspantiltmove(0, -10);
+        break;
+      case "leftspeed":
+        axis.ptz.continuouspantiltmove(-10, 0);
+        break;
+      case "rightspeed":
+        axis.ptz.continuouspantiltmove(10, 0);
+        break;
+      case "stopspeed":
+        axis.ptz.continuouspantiltmove(0, 0);
+        break;
+      case "zoominspeed":
+        axis.ptz.continuouszoommove(10);
+        break;
+      case "zoomoutspeed":
+        axis.ptz.continuouszoommove(-10);
+        break;
+      case "stopzoomspeed":
+        axis.ptz.continuouszoommove(0);
+        break;
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("A user has disconnected")
-    stream = {}
+    console.log("A user has disconnected");
   });
-
-
 });
 
 server.listen(port, hostname, () => {
--- src/App.js
+++ src/App.js
@@ -40,9 +40,9 @@ function App() {
 
   const [camera, setCamera] = useState({ ip: "", username: "", password: "" });
   const [tempcamera, setTempCamera] = useState({
-    ip: "",
-    username: "",
-    password: "",
+    ip: "212.170.116.46",
+    username: "root",
+    password: "pass",
   });
 
   const [flagCredentials, setFlagCredentials] = useState(true);
@@ -137,6 +137,11 @@ function App() {
     });
   }, []);
 
+  function editCredentials(){
+    setFlagCredentials(true)
+    socket.emit("deletestream");
+  }
+
   return (
     <>
       <div className="App">
@@ -187,118 +192,113 @@ function App() {
           </Form>
         ) : (
           <>
-            <Container className="d-flex flex-row w-100 h-100">
-              <Container className=" w-50 h-100" fluid>
-                <Row className="d-flex justify-content-center w-3 border mb-5"
-                  xs="12"
-                  lg="6"
-                  md="6">
-                  <Col className="w-100 d-flex justify-content-center">
-                    <Button
-                      onClick={() => setFlagCredentials(true)}
+            <Container className="d-flex flex-row w-100 h-100" fluid>
+            <Row className="w-100" >
+              {/* Video Column */}
+              <Col xs={12} sm={6} className="d-flex justify-content-center w-100">
+                  <div
+                    id="video-canvas"
+                    style={{height: "300px", width:"500px"}}
+                  ></div>
+                </Col>
+                {/* Video Column Close */}
+              
+                {/* Controls Column*/}
+                <Col xs={12} sm={6} className="d-flex justify-content-center w-100">
+                  {/* Edit Credentials Div */}
+                  <div className="w-100 h-100 mt-2">
+                    <div
+                      className="d-flex justify-content-center mb-3"
+                      xs="12"
+                      lg="6"
+                      md="6"
                     >
-                      Edit Credentials
-                    </Button>
-                  </Col>
-                </Row>
-
-                {/* Zoom row*/}
-
-                <Row
-                  className="d-flex mt-1 justify-content-center w-3 border"
-                  xs="12"
-                  lg="6"
-                  md="6"
-                >
-                  <Col className="w-100 d-flex justify-content-center">
-                    <Button
-                      variant="primary"
-                      style={{ width: "120px" }}
-                      onClick={(e) => handleButton(e, "zoomin")}
-                    >
-                      Zoom in
-                    </Button>
-                  </Col>
-                </Row>
-
-                {/* Joysticks row */}
-
-                <Row
-                  className="d-flex mt-3 justify-content-center border"
-                  xs="12"
-                  lg="6"
-                  md="6"
-                >
-                  <Col className=" w-100">
-                    <div className="d-flex justify-content-around">
-                      <Button
-                        style={{ width: "120px", height: "auto" }}
-                        onClick={(e) => handleButton(e, "up")}
-                      >
-                        Tilt Up
+                      <Button onClick={() => editCredentials()}>
+                        Edit Credentials
                       </Button>
                     </div>
 
-                    <Container className="w-100 border">
-                      <Row
-                        xs="12"
-                        lg="6"
-                        md="6"
-                        className="d-flex mt-2 justify-content-center"
-                      >
-                        <Col
-                          className="d-flex justify-content-center"
-                          style={{ width: "120px" }}
-                        >
-                          <Button
-                            style={{ width: "120px" }}
-                            onClick={(e) => handleButton(e, "left")}
-                          >
-                            Pan Left
-                          </Button>
-                        </Col>
-                        <Col
-                          className="d-flex justify-content-center"
-                          style={{ width: "120px" }}
-                        >
-                          <Button
-                            style={{ width: "120px" }}
-                            onClick={(e) => handleButton(e, "right")}
-                          >
-                            Pan Right
-                          </Button>
-                        </Col>
-                      </Row>
-                    </Container>
+                    {/* Zoomin div*/}
 
-                    <div className="d-flex justify-content-around mt-2">
+                    <div className="d-flex mt-1 justify-content-center w-100">
+                      <Button
+                        variant="primary"
+                        style={{ width: "120px" }}
+                        onClick={(e) => handleButton(e, "zoomin")}
+                      >
+                        Zoom in
+                      </Button>
+                    </div>
+
+                    {/* Joysticks div*/}
+
+                    <div className="d-flex flex-column mt-3 justify-content-center">
+                      {/* Tilt Up div */}
+                      <div className="d-flex justify-content-around w-100">
+                        <Button
+                          style={{ width: "120px", height: "auto" }}
+                          onClick={(e) => handleButton(e, "up")}
+                        >
+                          Tilt Up
+                        </Button>
+                      </div>
+                      {/* Pan Left/Right Container */}
+                      <Container className="w-100">
+                        <Row
+                          xs="12"
+                          lg="6"
+                          md="6"
+                          className="d-flex mt-2 justify-content-center"
+                        >
+                          <Col
+                            className="d-flex justify-content-center"
+                            style={{ width: "120px" }}
+                          >
+                            <Button
+                              style={{ width: "120px" }}
+                              onClick={(e) => handleButton(e, "left")}
+                            >
+                              Pan Left
+                            </Button>
+                          </Col>
+                          <Col
+                            className="d-flex justify-content-center"
+                            style={{ width: "120px" }}
+                          >
+                            <Button
+                              style={{ width: "120px" }}
+                              onClick={(e) => handleButton(e, "right")}
+                            >
+                              Pan Right
+                            </Button>
+                          </Col>
+                        </Row>
+                      </Container>
+                      {/* Tilt down div */}
+                      <div className="d-flex justify-content-around mt-2">
+                        <Button
+                          style={{ width: "120px" }}
+                          onClick={(e) => handleButton(e, "down")}
+                        >
+                          Tilt Down
+                        </Button>
+                      </div>
+                    </div>
+                    {/* Zoom out div */}
+                    <div className="d-flex justify-content-around mt-2 w-100">
                       <Button
                         style={{ width: "120px" }}
-                        onClick={(e) => handleButton(e, "down")}
+                        onClick={(e) => handleButton(e, "zoomout")}
                       >
-                        Tilt Down
+                        Zoom Out
                       </Button>
                     </div>
-                  </Col>
-                </Row>
-
-                <Row>
-                  <div className="d-flex justify-content-around mt-2 border">
-                    <Button
-                      style={{ width: "120px" }}
-                      onClick={(e) => handleButton(e, "zoomout")}
-                    >
-                      Zoom Out
-                    </Button>
                   </div>
-                </Row>
-              </Container>
-              <div
-                id="video-canvas"
-                style={{ height: "auto", minWidth: "640px" }}
-              ></div>
+                </Col>
+                {/* Controls Column Close */}
+                
+              </Row>
             </Container>
-
           </>
         )}
       </div>
