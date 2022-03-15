062b796bc175b9f5d9398f70366926e9a4977461
jonher16 <1999.jon@gmail.com>
2022-03-15 13:07:19 +0100

    Added Joystick Component

---
+++ src/components/GobeJoystickController.js
@@ -0,0 +1,49 @@
+import { Joystick } from "react-joystick-component";
+import { useEffect, useState, useMemo, useRef } from "react";
+
+function GobeJoystickController({
+    move,
+    start,
+    stop,
+    opactiy = 1.0,
+    className
+  }) {
+    const [containerDiv, setContainerDiv] = useState();
+    // the container will always fill it's parent
+    const containerStyle = useRef({
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyItems: "center",
+      justifyContent: "center"
+    }).current;
+    const baseColor = useMemo(
+      () =>
+        `radial-gradient(circle at 50% 50%, rgba(100,100,100,${opactiy}), rgba(100,100,100,${opactiy}), rgba(100,100,100,${opactiy}),  rgba(5,5,5,${opactiy}))`,
+      [opactiy]
+    );
+    const stickColor = useMemo(
+      () =>
+        `radial-gradient(circle at 50% 50%, rgba(70,70,70,${opactiy}), rgba(70,70,70,${opactiy}), rgba(5,5,5,${opactiy}))`,
+      [opactiy]
+    );
+    return (
+      <div ref={setContainerDiv} style={containerStyle} className={className}>
+        {containerDiv ? (
+          <Joystick
+            // we are assuming that the container dimensions will never change in the lifetime of this component
+            size={Math.min(containerDiv.offsetWidth, containerDiv.offsetHeight)}
+            baseColor={baseColor}
+            stickColor={stickColor}
+            throttle={200}
+            move={move}
+            stop={stop}
+            start={start}
+          />
+        ) : null}
+      </div>
+    );
+  }
+
+export default GobeJoystickController;
